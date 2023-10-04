d217c236b96a51324d025111d161a1bc13883784
Angelo Davis <francisdavia11@gmail.com>
2023-10-04 20:38:07 +0100

    Initial commit

---
+++ Source/PilotBlades/PilotBladesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PilotBladesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PILOTBLADES_API APilotBladesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
